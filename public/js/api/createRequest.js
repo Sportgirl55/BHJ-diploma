81712b8d7960e6c538b1f7f9b9ac2045a36ccd16
Elena Slyusareva <sportgirl55@bk.ru>
2020-11-18 16:48:00 +0500

    Initial commit

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -2,6 +2,44 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (options = {}) => {
+const createRequest = (options) => {
+  try {
+      let xhr = new XMLHttpRequest();
+      let formData = new FormData();
+      xhr.withCredentials = true;
 
+      xhr.addEventListener('onload', function () {
+
+      })
+    
+  } catch (e) {
+
+  }
 };
+
+
+
+
+// var xhr = new XMLHttpRequest();
+// xhr.open("GET", "/bar/foo.txt", true);
+// xhr.onload = function (e) {
+//   if (xhr.readyState === 4) {
+//     if (xhr.status === 200) {
+//       console.log(xhr.responseText);
+//     } else {
+//       console.error(xhr.statusText);
+//     }
+//   }
+// };
+// xhr.onerror = function (e) {
+//   console.error(xhr.statusText);
+// };
+// xhr.send(null); 
+
+
+
+
+
+
+
+
